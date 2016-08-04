b472ae4355eed22efaafce0a059fac2238b258ae
mahmoud17-meet <mahmoud17@meet.mit.edu>
2016-08-04 22:48:30 +0300

    n

--- database_setup.py
+++ database_setup.py
@@ -17,17 +17,6 @@ class Fact(Base):
 	tribe = Column(String)
 	statement = Column(String)
 	correct_answer = Column(String)
-	#f1 = Column(String)
-	#f2 = Column(String)
-	#f3 = Column(String)
-	#f4 = Column(String)
-	#f5 = Column(String)
-	#a1 = Column(String)
-	#a2 = Column(String)
-	#a3 = Column(String)
-	#a4 = Column(String)
-	#a5 = Column(String)
-
 
 class Answers(Base): 
 	__tablename__ = 'answers' 
@@ -35,17 +24,6 @@ class Answers(Base):
 	person_id = Column(Integer)
 	fact_id = Column(Integer)
 	answer = Column(String)
-	# tribes = Column(String)
-	# f1 = Column(String)
-	# f2 = Column(String)
-	# f3 = Column(String)
-	# f4 = Column(String)
-	# f5 = Column(String)
-	# a1 = Column(String)
-	# a2 = Column(String)
-	# a3 = Column(String)
-	# a4 = Column(String)
-	# a5 = Column(String)
-
+	
 
 #PLACE YOUR TABLE SETUP INFORMATION HERE
